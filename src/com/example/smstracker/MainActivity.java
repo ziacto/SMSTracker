04bd57f43a2a0364a753d74af6142442fce2e2a4
Rashitko <michal.raska@gmail.com>
2013-09-26 21:24:47 +0200

    init version

---
+++ src/com/example/smstracker/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.smstracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	
+	public static final String SUPER_TAG = "SMSTracker.";
+	public static final String TAG = SUPER_TAG + "MainActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void acquireLocation(View view) {
+		Log.d(TAG, "acquire location");
+	}
+
+}
