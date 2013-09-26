dac64e5e0d7ab1f59cc96c153c4fe2c18e6447bb
Rashitko <michal.raska@gmail.com>
2013-09-26 22:50:44 +0200

    basic gui, tracker not tested, logs for debugging, it SHOULD be ok

--- gen/com/example/smstracker/R.java
+++ gen/com/example/smstracker/R.java
@@ -22,13 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_tick=0x7f020001;
+        public static final int ic_warn=0x7f020002;
     }
     public static final class id {
-        public static final int acquireButton=0x7f080000;
         public static final int action_settings=0x7f080004;
+        public static final int imageView1=0x7f080003;
         public static final int progressBar1=0x7f080002;
-        public static final int sendButton=0x7f080003;
-        public static final int statusIcon=0x7f080001;
+        public static final int sendButton=0x7f080000;
+        public static final int signIcon=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/smstracker/MainActivity.java
+++ src/com/example/smstracker/MainActivity.java
@@ -1,31 +1,91 @@
 package com.example.smstracker;
 
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-public class MainActivity extends Activity {
-	
+public class MainActivity extends Activity implements LocationNeeder {
+
+	private static final String LOCATION_NOT_ACQUIRED_YET = "L·ska, je to pomalÈ, strpenie prosÌm.\nInak vieö, ûe ùa milujem :)?";
 	public static final String SUPER_TAG = "SMSTracker.";
-	public static final String TAG = SUPER_TAG + "MainActivity";
+	private static final String TAG = SUPER_TAG + "MainActivity";
+	private static final CharSequence NO_GPS_FIX = "Eöte nechytil satelity";
+	private ProgressBar progressBar;
+	private Tracker tracker;
+	private Location location;
+	private ImageView signIcon;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initViewFields();
+		tracker = new Tracker(this, this);
+	}
+
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "acquire location");
+		progressBar.setVisibility(View.VISIBLE);
+		location = null;
+		tracker.acquireLocation();
+	}
+
+	protected void onPause() {
+		super.onPause();
+		tracker.cancelUpdates();
+	}
+
+	private void initViewFields() {
+		progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		signIcon = (ImageView) findViewById(R.id.signIcon);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void acquireLocation(View view) {
-		Log.d(TAG, "acquire location");
+
+	public void sendCoords(View view) {
+		Log.d(TAG, "send sms");
+		if (location != null) {
+			send();
+		} else {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(NO_GPS_FIX);
+			builder.setMessage(LOCATION_NOT_ACQUIRED_YET);
+			builder.setPositiveButton(android.R.string.ok, null);
+			builder.show();
+		}
+	}
+
+	@Override
+	public void locationChanged(Location location) {
+		Log.d(TAG, "locationChanged");
+		progressBar.setVisibility(View.INVISIBLE);
+		this.location = location;
+	}
+
+	private void send() {
+		String number = "+421907351787";
+		String text = getSMSText();
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts(text,
+				number, null));
+		startActivity(intent);
+	}
+
+	private String getSMSText() {
+		return "sms text";
 	}
 
 }
